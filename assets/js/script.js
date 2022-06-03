036465cbe3e696311b7bb012350c7e4655c0ca31
Potwora <matthewpotwora@yahoo.com>
2022-06-02 18:18:15 -0700

    showing my mom how to save

--- assets/js/script.js
+++ assets/js/script.js
@@ -277,6 +277,11 @@ $("#remove-tasks").on("click", function() {
   console.log(tasks);
   saveTasks();
 });
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000*60)*30);
 
 // load tasks for the first time
 loadTasks();
